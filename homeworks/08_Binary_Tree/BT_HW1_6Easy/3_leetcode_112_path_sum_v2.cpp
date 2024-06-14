ec8a5c0672dd54e2046d0bf012f9f17f1b0f46f1
Ahmed Elgarnousy <ahmedgarnousy76@gmail.com>
2024-06-14 12:44:44 +0300

    leetcode 112 path sum v2

--- homeworks/08_Binary_Tree/BT_HW1_6Easy/3_leetcode_112_path_sum_v2.cpp
+++ homeworks/08_Binary_Tree/BT_HW1_6Easy/3_leetcode_112_path_sum_v2.cpp
@@ -4,34 +4,40 @@
 #include <vector>
 using namespace std;
 
-
-struct TreeNode {		// don't copy for leetcode
-	int val { };
-	TreeNode *left { };
-	TreeNode *right { };
-	TreeNode(int val) :
-			val(val) {
+struct TreeNode
+{ // don't copy for leetcode
+	int val{};
+	TreeNode *left{};
+	TreeNode *right{};
+	TreeNode(int val) : val(val)
+	{
 	}
 };
 
-struct BinaryTree {
-	TreeNode *root { };
-	BinaryTree(int root_value) :
-			root(new TreeNode(root_value)) {
+struct BinaryTree
+{
+	TreeNode *root{};
+	BinaryTree(int root_value) : root(new TreeNode(root_value))
+	{
 	}
 
-	void add(vector<int> values, vector<char> direction) {
+	void add(vector<int> values, vector<char> direction)
+	{
 		assert(values.size() == direction.size());
 		TreeNode *current = this->root;
 		// iterate on the path, create all necessary TreeNodes
-		for (int i = 0; i < (int) values.size(); ++i) {
-			if (direction[i] == 'L') {
+		for (int i = 0; i < (int)values.size(); ++i)
+		{
+			if (direction[i] == 'L')
+			{
 				if (!current->left)
 					current->left = new TreeNode(values[i]);
 				else
 					assert(current->left->val == values[i]);
 				current = current->left;
-			} else {
+			}
+			else
+			{
 				if (!current->right)
 					current->right = new TreeNode(values[i]);
 				else
@@ -41,7 +47,8 @@ struct BinaryTree {
 		}
 	}
 
-	void _printInorder(TreeNode *current) {
+	void _printInorder(TreeNode *current)
+	{
 		if (!current)
 			return;
 		_printInorder(current->left);
@@ -49,30 +56,43 @@ struct BinaryTree {
 		_printInorder(current->right);
 	}
 
-	void printInorder() {
+	void printInorder()
+	{
 		_printInorder(root);
 		cout << "\n";
 	}
 };
 
-
-
-class Solution {
+bool isLeaf(TreeNode *node)
+{
+	return node && !node->left && !node->right;
+}
+class Solution
+{
 public:
-	
+	bool hasPathSum(TreeNode *root, int targetSum)
+	{
+		if (!root)
+			return 0;
+
+		if (isLeaf(root))
+			return targetSum == root->val;
+
+		return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+	}
 };
 
-int main() {	// rename like main1 for leetcode
+int main()
+{ // rename like main1 for leetcode
 	BinaryTree tree(1);
-	tree.add( { 2, 4, 7 }, { 'L', 'L', 'L' });
-	tree.add( { 2, 4, 8 }, { 'L', 'L', 'R' });
-	tree.add( { 2, 5, 9 }, { 'L', 'R', 'R' });
-	tree.add( { 3, 6, 10 }, { 'R', 'R', 'L' });
+	tree.add({2, 4, 7}, {'L', 'L', 'L'});
+	tree.add({2, 4, 8}, {'L', 'L', 'R'});
+	tree.add({2, 5, 9}, {'L', 'R', 'R'});
+	tree.add({3, 6, 10}, {'R', 'R', 'L'});
 
-	// cout << Solution().hasPathSum(tree.root, 17) << "\n";
-	// cout << Solution().hasPathSum(tree.root, 170) << "\n";
-	// cout << Solution().hasPathSum(tree.root, 0) << "\n";
+	cout << Solution().hasPathSum(tree.root, 17) << "\n";
+	cout << Solution().hasPathSum(tree.root, 170) << "\n";
+	cout << Solution().hasPathSum(tree.root, 0) << "\n";
 
 	return 0;
 }
-
